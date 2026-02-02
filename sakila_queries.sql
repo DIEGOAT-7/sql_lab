64d0bdc2533608737b9f7720828b224bfc006504
Diego Ortiz <diegoortizare@gmail.com>
2026-02-01 23:09:16 -0500

    New Queries in Sakila

--- sakila_queries.sql
+++ sakila_queries.sql
@@ -17,3 +17,36 @@ FROM language;
 SELECT version(),
   user(),
   database();
+
+---
+
+SELECT a.actor_id,
+       a.first_name,
+       a.last_name,
+       c.category_id,
+       c.name,
+       co.country_id,
+       co.country
+FROM actor AS a
+JOIN category AS c
+ON category_id
+JOIN country AS co 
+ON country_id
+
+---
+
+SELECT DISTINCT
+    a.actor_id,
+    a.first_name,
+    a.last_name,
+    c.category_id,
+    c.name AS category_name
+FROM actor a
+JOIN film_actor fa
+    ON a.actor_id = fa.actor_id
+JOIN film f
+    ON fa.film_id = f.film_id
+JOIN film_category fc
+    ON f.film_id = fc.film_id
+JOIN category c
+    ON fc.category_id = c.category_id;
