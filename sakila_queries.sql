7005808d4e4734142cccf0aefb434bc98e44310e
Diego Ortiz <diegoortizare@gmail.com>
2026-02-06 15:14:23 -0500

    adding queires

--- sakila_queries.sql
+++ sakila_queries.sql
@@ -17,21 +17,14 @@ FROM language;
 SELECT version(),
   user(),
   database();
+--
 
----
-
-SELECT a.actor_id,
-       a.first_name,
-       a.last_name,
-       c.category_id,
-       c.name,
-       co.country_id,
-       co.country
-FROM actor AS a
-JOIN category AS c
-ON category_id
-JOIN country AS co 
-ON country_id
+SELECT concat(cust.last_name, ', ', cust.first_name) full_name
+FROM
+    (SELECT first_name, last_name, email
+    FROM customer
+    WHERE first_name = 'JESSIE'
+    ) cust;
 
 ---
 
@@ -50,3 +43,17 @@ JOIN film_category fc
     ON f.film_id = fc.film_id
 JOIN category c
     ON fc.category_id = c.category_id;
+
+----
+
+-- ver todo el mapa relacional de la base de datos 
+
+SELECT
+    table_name,
+    column_name,
+    referenced_table_name,
+    referenced_column_name
+FROM information_schema.KEY_COLUMN_USAGE
+WHERE table_schema = DATABASE()
+  AND referenced_table_name IS NOT NULL
+ORDER BY table_name;
